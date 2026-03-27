e7190201b1cda440d11d874bef4dc49846f22908
Mário César <71349316+mariocesarfilho@users.noreply.github.com>
2026-03-27 12:28:30 +0000

    modifie `database.js` file

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
     const client = await new Client({
-        host: process.env.POSTGRESS_HOST,
-        port: process.env.POSTGRESS_PORT,
-        user: process.env.POSTGRESS_USER,
-        database: process.env.POSTGRESS_DB,
-        password: process.env.POSTGRESS_PASSWORD 
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD
     }).connect()
     const res = await client.query(queryObject);    
     await client.end()

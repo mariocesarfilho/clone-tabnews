5449ee797ffa8027c1ca91a5868bb8a059b2fc78
Mário César <71349316+mariocesarfilho@users.noreply.github.com>
2026-03-27 17:04:26 +0000

    atualiza a função de status para retornar a data de atualização

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,12 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-    const res = await database.query('SELECT 1 + 1 as sum;');
-    console.log(res.rows);
-    response.status(200).json({ chave: "são acima da média" });
+    const updateAt = new Date().toISOString();
+
+    response.status(200).json({
+        update_at: updateAt,
+    });
+        
 };
 
 export default status;

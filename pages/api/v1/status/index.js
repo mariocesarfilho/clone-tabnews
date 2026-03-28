d20319f07bcf5c3aa046cde37c0f8f9286625883
Mário César <71349316+mariocesarfilho@users.noreply.github.com>
2026-03-28 22:18:08 +0000

    modifie `status.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,27 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-    const updateAt = new Date().toISOString();
+    const updatedAt = new Date().toISOString();
+
+    const dbValueResult = await database.query("SHOW server_version;");
+    const dbVersionValue = dbValueResult.rows[0].server_version;
+
+    const dbMaxConnection = await database.query("SHOW max_connections;");
+    const dbMaxConnectionValue = dbMaxConnection.rows[0].max_connections;
+
+    const dbOpenedConnectionResult = await database.query("SELECT * pg_stat_activity");
+    // console.log(dbOpenedConnectionResult);
+
+    console.log("Response Status: " ,response.status);
 
     response.status(200).json({
-        update_at: updateAt,
+        update_at: updatedAt,
+        dependencies: {
+            database: {
+                version: dbVersionValue,
+                max_connections: parseInt(dbMaxConnectionValue),
+            },
+        },
     });
         
 };

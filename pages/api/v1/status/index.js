647340082a2908f041f1adc50ea57738da2ee694
Mário César <71349316+mariocesarfilho@users.noreply.github.com>
2026-04-20 20:29:25 +0000

    refatora a função de status para melhorar a legibilidade e corrige a consulta de conexões abertas

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,14 +3,16 @@ import database from "infra/database.js"
 async function status(request, response) {
     const updatedAt = new Date().toISOString();
 
+    
     const dbValueResult = await database.query("SHOW server_version;");
     const dbVersionValue = dbValueResult.rows[0].server_version;
-
+    
     const dbMaxConnection = await database.query("SHOW max_connections;");
     const dbMaxConnectionValue = dbMaxConnection.rows[0].max_connections;
-
-    const dbOpenedConnectionResult = await database.query("SELECT * pg_stat_activity");
-    // console.log(dbOpenedConnectionResult);
+    
+    const databaseName = "postgres"
+    const dbOpenedConnectionResult = await database.query(
+        `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`);    
 
     console.log("Response Status: " ,response.status);
 
